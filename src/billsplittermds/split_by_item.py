6cabe8f22509bcd009d8e4dca6c2a07c77b34779
Jessie Liang <rnliang.jessie@gmail.com>
2026-01-08 06:32:27 -0800

    function 2 docstring done

--- src/billsplittermds/split_by_item.py
+++ src/billsplittermds/split_by_item.py
@@ -5,6 +5,36 @@ import pandas as pd
 
 def split_by_item(valid_df):
     """
+    Calculates a derived column called individual_price, which is the amount 
+    of this item that an individual should pay after splitting the bill evenly among 
+    all people who consumed this item. Then we sum each person’s individual_price
+    to get the total amount that this individual should pay. In brief, this function calculates
+    how much each individual needs to pay in total during the trip after splitting the bill
+    by item.
 
+    Parameters
+    ----------
+    valid_df : pandas.DataFrame
+        A dataframe after being validated with columns 'payer', 
+        'item_name', 'item_price', 'shared_by', 'tax_pct', and 'tip_pct'.
+
+    Returns
+    -------
+    should_pay_df : pandas.DataFrame
+        Dataframe with columns 'individual' and 'should_pay'.
+
+    Examples
+    --------
+    >>> valid_df
+        payer   item_name  item_price  shared_by  tax_pct  tip_pct
+    0   Leo     candy      10.0        Leo        0.12     0.15
+    1   Leo     taxi       25.0        Leo, Ana   0.07     0.0
+    2   Ana     lunch      20.0        Ana        0.12     0.15
+
+    >>> split_by_item(valid_df)
+        individual   should_pay
+    0   Leo          26.255
+    1   Ana          39.135
+    
     """
     pass
